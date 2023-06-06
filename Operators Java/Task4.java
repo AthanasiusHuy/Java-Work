46fbeae7dcc9498d4fc74ebd406248add5d9ea64
Huy Le <68962322+AthanasiusHuy@users.noreply.github.com>
2023-06-06 22:37:13 +1000

    Task4.java

--- Operators Java/Task4.java
+++ Operators Java/Task4.java
@@ -1,27 +1,21 @@
 public class Task4 {
     public static void main(String[] args) {
-        // bai 4
         int length = 5;
         int width = 7;
-
         int perimeter = 0;
         int area = 1;
         perimeter += 2 * length;
         perimeter += 2 * width;
         area *= length;
         area *= width;
-
         System.out.println("PERIMETER: " + perimeter + " " + "AREA: " + area);
-
         int a = 2;
         int b = 3;
         int c = 5;
-
         perimeter = 0;
         perimeter += a;
         perimeter += b;
         perimeter += c;
-
         System.out.println("PERIMETER " + perimeter);
     }
 }
